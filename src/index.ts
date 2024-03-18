07ebfd0850d7d0c6e93d5f2a9ec435f4507b9fb0
Pavel <magma4810@gmail.com>
2024-03-18 13:07:08 +0300

    improved menu

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,21 @@ import "./stylesSlider.css";
 import "./style.css";
 import "./slider";
 
-const headerList = document.querySelector(".header-list");
-const headerBurger = document.querySelector(".header-burger");
+const headerList: Element | null = document.querySelector(".header-list");
+const headerBurger: Element | null = document.querySelector(".header-burger");
+const headerLink: NodeListOf<Element> = document.querySelectorAll(".header-link");
+const headerPhone: Element | null = document.querySelector(".header-phone");
+const elementsArray: Array<Element> = Array.from(headerLink);
 
 headerBurger?.addEventListener("click", () => {
   headerList!.classList.toggle("active");
   headerBurger!.classList.toggle("active");
+  headerPhone!.classList.toggle("active");
   document.body.classList.toggle("lock");
 });
+
+elementsArray!.map(el => el.addEventListener('click',() => {
+  headerList!.classList.remove("active");
+  headerBurger!.classList.remove("active");
+  document.body.classList.remove("lock");
+}))
