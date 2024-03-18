3e8bce6500a4b3de92737bcf7e642b730ffae5ef
Pavel <magma4810@gmail.com>
2024-03-18 11:38:17 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ import "./stylesSlider.css";
 import "./style.css";
 import "./slider";
 
-const headerList = document.querySelector('.header-list');
-const headerBurger = document.querySelector('.header-burger');
+const headerList = document.querySelector(".header-list");
+const headerBurger = document.querySelector(".header-burger");
 
-headerBurger?.addEventListener('click', () => {
-  headerList!.classList.toggle('active');
-  headerBurger!.classList.toggle('active');
-  document.body.classList.toggle('lock');
-})
+headerBurger?.addEventListener("click", () => {
+  headerList!.classList.toggle("active");
+  headerBurger!.classList.toggle("active");
+  document.body.classList.toggle("lock");
+});
